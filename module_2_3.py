b0ecae9ae07dc71a715120490a94bc7eb0194990
skamkin <doc.kamkin@gmail.com>
2024-06-20 23:14:45 +0500

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+i = 0
+while i < len(my_list):
+    if my_list[i]>0:
+        print(my_list[i])
+        i += 1
+    elif my_list[i]==0:
+        i += 1
+    else:
+        break
+
+
